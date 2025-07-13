220124c44e4e9104e8ec48bcd46e47966995c47a
alitolga0 <cakiralitolga@gmail.com>
2025-07-13 17:35:01 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+}
+</style>
